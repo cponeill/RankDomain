e87fc723b5190d36913bc94cb05e082fb208c8b8
cponeill <caseyoneill78@hotmail.com>
2017-01-27 11:25:29 -0500

    Changing some of the code and adding comments.

--- client.py
+++ client.py
@@ -1,6 +1,10 @@
 """
-Copyright Blockshare Technologies, LLC.
-
+Copyright (c) 2016-2017 Blockshare Technologies, LLC.
+  ____  _            _     ____  _                      ___ ___  
+ | __ )| | ___   ___| | __/ ___|| |__   __ _ _ __ ___  |_ _/ _ \ 
+ |  _ \| |/ _ \ / __| |/ /\___ \| '_ \ / _` | '__/ _ \  | | | | |
+ | |_) | | (_) | (__|   <  ___) | | | | (_| | | |  __/_ | | |_| |
+ |____/|_|\___/ \___|_|\_\|____/|_| |_|\__,_|_|  \___(_)___\___/ 
 """
 
 __author__ = "cponeill"
@@ -18,7 +22,7 @@ from two1.bitrequests import BitTransferRequests
 wallet = Wallet()
 requests = BitTransferRequests(wallet)
 
-server_url = 'http://[::]:<port>/'
+server_url = 'http://[fcce:a977:eee3:6ebf:1f21:0000:0000:0001]:7002/'
 
 def get_rank():
     """Return JSON-encoded output of Alexa ranking for 
