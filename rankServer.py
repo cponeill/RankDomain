1f5256b6381eb23f482023ee427c0aaf7937a241
cponeill <caseyoneill78@hotmail.com>
2017-01-27 11:26:57 -0500

    Renaming server, changing URL to IPV6 address, and updating comment code.

---
+++ rankServer.py
@@ -0,0 +1,62 @@
+"""
+Copyright Blockshare Technologies, LLC.
+
+"""
+
+__author__ = "cponeill"
+__version__ = "1.0"
+__maintainer__ = "cponeill"
+__email__ = "cponeill@blockshare.io"
+
+
+import requests
+import json
+
+from flask import Flask, request
+from two1.wallet import Wallet
+from two1.bitserv.flask import Payment
+from xml.etree import ElementTree
+
+
+app = Flask(__name__)
+wallet = Wallet()
+payment = Payment(app, wallet)
+
+
+def get_alexa_xml(args):
+    """
+    Abstract function that pulls in xml data from url
+    and returns that data.
+    """
+    response = requests.get('http://data.alexa.com/data?cli=10&url=http://' + args)
+    tree = ElementTree.fromstring(response.text)
+    return tree
+
+
+@app.route('/domain_rank')
+@payment.required(750)
+def get_rank():
+    """
+    Input: Specific URL.
+    Output: JSON-encoded Alexa rankings of URL.
+    Exception raised if domain not found in rankings or does not rank high enough.
+    """
+    url = request.args.get('url')
+    
+    try:
+        rank = get_alexa_xml(url).find(".//REACH").get("RANK")
+        delta = get_alexa_xml(url).find(".//RANK").get("DELTA")
+        params = {
+            'domain-rank': {
+                'rank': rank,
+                'rank-change': delta,
+                'url': url
+            }
+        }
+        return json.dumps(params, indent=2)
+    except:
+        params = {'alexa-ranking': {'ranking': 'this domain is not ranked'}}
+        return json.dumps(params, indent=2)
+
+if __name__ == '__main__':
+    app.run(host="::", port=7002)
