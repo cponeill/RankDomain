53c947f53fd508911c8d221d3897e8118c1625c5
cponeill <caseyoneill78@hotmail.com>
2017-01-29 12:26:50 -0500

    Updating commented copy.

--- tests.py
+++ tests.py
@@ -11,14 +11,17 @@ from unittest import mock
 
 class RankDomainTestCase(unittest.TestCase):
 
+    # Get the url from the server application.
     def get(self, url):
         self.app = rankServer.app.test_client()
         return self.app.get(url)
 
+    # Test whether the get command requests the 402 endpoint.
     def test_success(self):
         response = self.get('/domain_rank')
         self.assertEqual(response.status_code, 402)
 
+    # Test whether the get command requests the 402 endpoint and gets a 200 status update.
     @mock.patch('two1.bitserv.flask.decorator.Payment.contains_payment',
                  return_value=True)
     def test_pay_success(self, *args):
