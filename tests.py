8bf155bf1d63dfc30bf11b12a6b7ec146cdaa7ec
cponeill <caseyoneill78@hotmail.com>
2017-02-02 10:38:58 -0500

    keeping tests.py updatedto pep8 standards.

--- tests.py
+++ tests.py
@@ -1,6 +1,4 @@
-# TO DO: Add comments before each function to describe what is happening. 
-
-#!/usr/bin/env python3
+#! /usr/bin/env python3
 # Adding in unittests RankDomain microservice.
 import os
 import unittest
@@ -9,6 +7,7 @@ import json
 
 from unittest import mock
 
+
 class RankDomainTestCase(unittest.TestCase):
 
     # Get the url from the server application.
@@ -16,17 +15,18 @@ class RankDomainTestCase(unittest.TestCase):
         self.app = rankServer.app.test_client()
         return self.app.get(url)
 
-    # Test whether the get command requests the 402 endpoint.
+    # Get 402 status.
     def test_success(self):
         response = self.get('/domain_rank')
         self.assertEqual(response.status_code, 402)
 
-    # Test whether the get command requests the 402 endpoint and gets a 200 status update.
+    # Get 200 status.
     @mock.patch('two1.bitserv.flask.decorator.Payment.contains_payment',
                  return_value=True)
     def test_pay_success(self, *args):
         response = self.get('/domain_rank?url=google.com')
         self.assertEqual(response.status_code, 200)
+
 
 if __name__ == '__main__':
     unittest.main()
